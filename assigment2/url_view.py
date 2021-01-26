17a704eb01bbc7765dbe071dab21cfce119b33f3
Darya Belyavskaya <darya.belyavskaya.05@mail.ru>
2021-01-26 13:37:48 +0300

    Change static method to function and rename.

--- assigment2/url_view.py
+++ assigment2/url_view.py
@@ -16,7 +16,6 @@ FILE_NAME = f'reddit-{get_time()}.txt'
 
 
 def get_id(url):
-    print(re.match(r'/posts/(.+)/', url).group(1))
     return re.match(r'/posts/(.+)/', url).group(1)  # id
 
 
@@ -28,64 +27,61 @@ def check_not_empty_file():
     return os.path.getsize(FILE_NAME) > 0
 
 
-class UrlView:
+def get_file_data(url=None, vars=None):
+    if os.path.isfile(FILE_NAME) and check_not_empty_file():
+        with open(FILE_NAME, 'r') as f:
+            data = load_data_file(f)
+            return ResponseStatus(200, 'application/json', data)
+    return ResponseStatus(404, 'application/json', {})
 
-    @staticmethod
-    def do_GET_json(url=None, vars=None):
-        if os.path.isfile(FILE_NAME) and check_not_empty_file():
-            with open(FILE_NAME, 'r') as f:
-                data = load_data_file(f)
-                return ResponseStatus(200, 'application/json', data)
-        return ResponseStatus(404, 'application/json', {})
 
-    @staticmethod
-    def do_GET(url=None, vars=None):
-        if os.path.isfile(FILE_NAME) and check_not_empty_file():
-            with open(FILE_NAME, 'r') as f:
-                posts_data = load_data_file(f)
-                for post in posts_data:
-                    if post['uniqueId'] == get_id(url):
-                        return ResponseStatus(200, 'application/json', posts_data[post])
-        return ResponseStatus(404, 'application/json', {})
+def get_post(url=None, vars=None):
+    if os.path.isfile(FILE_NAME) and check_not_empty_file():
+        with open(FILE_NAME, 'r') as f:
+            posts_data = load_data_file(f)
+            for post in posts_data:
+                if post['uniqueId'] == get_id(url):
+                    return ResponseStatus(200, 'application/json', posts_data[post])
+    return ResponseStatus(404, 'application/json', {})
 
-    @staticmethod
-    def do_POST(url=None, vars=None):
+
+def add_post(url=None, vars=None):
+    with open(FILE_NAME, 'w+') as f:
+        posts_data = []
+        if check_not_empty_file():
+            posts_data = load_data_file(f)
+            for post in posts_data:
+                if post['uniqueId'] == vars['uniqueId']:
+                    return ResponseStatus(400, 'application/json', {})
+        posts_data.append(vars)
+        f.write(json.dumps(posts_data))
+        return ResponseStatus(201, 'application/json', {"uniqueId": vars['uniqueId']})
+
+
+def update_post(url=None, vars=None):
+    if os.path.isfile(FILE_NAME):
         with open(FILE_NAME, 'w+') as f:
-            posts_data = []
             if check_not_empty_file():
                 posts_data = load_data_file(f)
-                for post in posts_data:
-                    if post['uniqueId'] == vars['uniqueId']:
-                        return ResponseStatus(400, 'application/json', {})
-            posts_data.append(vars)
-            f.write(json.dumps(posts_data))
-            return ResponseStatus(201, 'application/json', {"uniqueId": vars['uniqueId']})
-
-    @staticmethod
-    def do_PUT(url=None, vars=None):
-        if os.path.isfile(FILE_NAME):
-            with open(FILE_NAME, 'w+') as f:
-                if check_not_empty_file():
-                    posts_data = load_data_file(f)
-                    for post_index in range(len(posts_data)):
-                        if posts_data[post_index]['uniqueId'] == get_id(url):
-                            posts_data[post_index] = vars
-                            f.write(json.dumps(posts_data))
-                            return ResponseStatus(200, 'application/json', {})
-        return ResponseStatus(404, 'application/json', {})
-
-    @staticmethod
-    def do_DELETE(url=None, vars=None):
-        if os.path.isfile(FILE_NAME):
-            with open(FILE_NAME, 'r+') as f:
-                if check_not_empty_file():
-                    posts_data = load_data_file(f)
                 for post_index in range(len(posts_data)):
                     if posts_data[post_index]['uniqueId'] == get_id(url):
-                        del posts_data[post_index]
-                        print(posts_data)
-                        f.seek(0)
-                        f.truncate(0)
+                        posts_data[post_index] = vars
                         f.write(json.dumps(posts_data))
-                        return ResponseStatus(204, 'application/json', {})
-        return ResponseStatus(404, 'application/json', {})
+                        return ResponseStatus(200, 'application/json', {})
+    return ResponseStatus(404, 'application/json', {})
+
+
+def remove_post(url=None, vars=None):
+    if os.path.isfile(FILE_NAME):
+        with open(FILE_NAME, 'r+') as f:
+            if check_not_empty_file():
+                posts_data = load_data_file(f)
+            for post_index in range(len(posts_data)):
+                if posts_data[post_index]['uniqueId'] == get_id(url):
+                    del posts_data[post_index]
+                    print(posts_data)
+                    f.seek(0)
+                    f.truncate(0)
+                    f.write(json.dumps(posts_data))
+                    return ResponseStatus(204, 'application/json', {})
+    return ResponseStatus(404, 'application/json', {})
